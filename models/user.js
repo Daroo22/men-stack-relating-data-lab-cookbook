b3dbddaee5cc1b1fa300759518765300cf264914
daroo22 <user@email.com>
2024-08-01 16:58:44 -0400

    progress

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
+
+import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
+
   },
   password: {
     type: String,
@@ -11,6 +13,6 @@ const userSchema = mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+export default User
--- server.js
+++ server.js
@@ -1,48 +1,54 @@
-const dotenv = require('dotenv');
+import dotenv from 'dotenv'
+import express from 'express';
+import mongoose from 'mongoose';
+import methodOverride from 'method-override';
+import morgan from 'morgan';
+import session from 'express-session';
+
+import authController from './controllers/auth.js';
+
 dotenv.config();
-const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-const session = require('express-session');
 
-const authController = require('./controllers/auth.js');
-
-const port = process.env.PORT ? process.env.PORT : '3000';
+// Set the port from environment variable or default to 3000
+const port = process.env.PORT || "3000";
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
+mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+// Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride("_method"));
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
+// Add Session middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+}))
 
-app.get('/', (req, res) => {
+// Routes
+app.get('/', async (req, res) => {
   res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
+    user: req.session.user
+  })
+})
 
-app.get('/vip-lounge', (req, res) => {
+app.get('/vip-lounge', async (req, res) => {
   if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
+    res.send(`Welcome to the party ${req.session.user.username}`)
   }
-});
+  else {
+    res.redirect('/auth/sign-in')
+  }
+})
 
-app.use('/auth', authController);
+app.use('/auth', authController)
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
